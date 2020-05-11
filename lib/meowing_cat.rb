a7fdaf79d1a2a34370fa86a3f1011811a514b9b9
Jared  E <jared.epps322@gmail.com>
2020-05-11 17:14:10 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,8 +1,16 @@
+# class Cat 
+#   attr_accessor :name 
+  
+#   def meow 
+#     puts "meow!"
+#   end 
+# end 
+  
+
 class Cat 
   attr_accessor :name 
   
-  def meow 
+  def meow
     puts "meow!"
   end 
 end 
-  
